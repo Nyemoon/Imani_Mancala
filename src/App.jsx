c1bf851a971865c9c97215748648088db4cf996d
Ketlyn Barbosa <kbl1@aluno.ifal.edu.br>
2026-04-15 22:59:03 -0300

    Estrutura base do jogo Mancala. Regras do jogo funcionando normalmente no JS

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import MancalaGame from './MancalaGame';
+
+const App = () => {
+  return (
+    <div className="app-shell">
+      <MancalaGame />
+    </div>
+  );
+};
+
+export default App;
